b8f87a4a17dc72eecc6cf238556026750aa5689f
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-06-22 10:15:08 -0600

    Add places

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["TJ", "Jake", "Chris", "Brett", "Stacy"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["NYC", "SF", "London", "Atlanta", "Denver"]
+places.pop()
+console.log(places)
